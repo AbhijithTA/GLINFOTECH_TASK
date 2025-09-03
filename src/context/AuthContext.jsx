7e5d68ea6e1c9a7f729f67e5c4b4b0024301a6d6
abhijith T A <abhijithajith13@gmail.com>
2025-09-03 12:28:27 +0530

    built error

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
-import { auth } from "../firebaseConfig";
+import { auth } from "../fireBaseConfig";
 
 const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
